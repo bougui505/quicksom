0bb3644f4ab154c8a628bec5eb14237ffac8615e
Guillaume Bouvier <bougui@mantrisse.bis.pasteur.fr>
2020-09-15 10:45:21 +0200

    Load data as a npy file 	modified:   quicksom_predict.py

--- bin/quicksom_predict.py
+++ bin/quicksom_predict.py
@@ -9,7 +9,7 @@ import argparse
 
 parser = argparse.ArgumentParser()
 # In/Out
-parser.add_argument("-i", "--in_name", default=None, help="name of the txt to use")
+parser.add_argument("-i", "--in_name", default=None, help="name of the npy file to use")
 parser.add_argument("-o", "--out_name", default='out.txt', help="name of txt to dump")
 parser.add_argument("-s", "--som_name", default='som.p', help="name of pickle to load")
 parser.add_argument("--recompute_cluster", default=False, action='store_true', help="if set, periodic topology is used")
@@ -21,9 +21,7 @@ if args.recompute_cluster:
 
 if args.in_name is not None:
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    X = numpy.genfromtxt(args.in_name)
-    # y = X[:, 2]
-    # X = X[:, :2]
+    X = numpy.load(args.in_name)
     X = torch.from_numpy(X)
     X = X.float()
     X = X.to(device)
